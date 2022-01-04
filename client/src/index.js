533b65a08430a9a79e678eb5f99f6e2446024d18
sazzad47 <sazzad47.ju@gmail.com>
2022-01-04 23:56:31 +0600

    make it better

--- client/src/index.js
+++ client/src/index.js
@@ -1,28 +1,24 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
-import { hydrate, render } from "react-dom";
 import './styles/global.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import DataProvider from './redux/store'
 import { HelmetProvider } from 'react-helmet-async';
 
-const APP = (
-<HelmetProvider>
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <HelmetProvider>
     <DataProvider>
       <App />
     </DataProvider>
-  </HelmetProvider>
+    </HelmetProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-const rootElement = document.getElementById("root");
-if (rootElement.hasChildNodes()) {
-  hydrate(APP, rootElement);
-} else {
-  render(APP, rootElement);
-}
-
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
