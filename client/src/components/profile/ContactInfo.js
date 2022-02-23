f91afe3473f2249c52c38070d3dd2c3f089080f9
sazzad47 <sazzad47.ju@gmail.com>
2022-02-24 04:50:27 +0600

    make it better

--- client/src/components/profile/ContactInfo.js
+++ client/src/components/profile/ContactInfo.js
@@ -26,17 +26,17 @@ const ContactInfo = ({id, dispatch, user, profile}) => {
                <div className="col-md-6 w-50 w-lg-50 cAnswer"><div className="bioInfoA">{user.fullname}</div> </div>
              </div>}
         {user.gMobile === ""? null :
-             <div className="row infoBox-1">
+             <div className="row infoBox-2">
                <div className="col-md-6 w-50 w-lg-50 wQuestion"><div className="bioInfoQ">অভিভাবকের মোবাইল নাম্বার</div> </div>
                <div className="col-md-6 w-50 w-lg-50 cAnswer"><div className="bioInfoA">{user.gMobile}</div> </div>
              </div>}
         {user.gRelation === ""? null :
-             <div className="row infoBox-2">
+             <div className="row infoBox-1">
                <div className="col-md-6 w-50 w-lg-50 wQuestion"><div className="bioInfoQ">{user.gender === "পাত্রের বায়োডাটা"?<span>পাত্র যার নাম্বার দিয়েছেন</span>:<span>পাত্রী যার নাম্বার দিয়েছেন</span>}</div> </div>
                <div className="col-md-6 w-50 w-lg-50 cAnswer"><div className="bioInfoA">{user.gRelation}</div> </div>
              </div>}
         {user.urEmail === ""? null :
-             <div className="row infoBox-1">
+             <div className="row infoBox-2">
                <div className="col-md-6 w-50 w-lg-50 wQuestion"><div className="bioInfoQ">বায়োডাটা পাঠানোর ই-মেইল এড্রেস</div> </div>
                <div className="col-md-6 w-50 w-lg-50 cAnswer"><div className="bioInfoA">{user.urEmail}</div> </div>
              </div>}
